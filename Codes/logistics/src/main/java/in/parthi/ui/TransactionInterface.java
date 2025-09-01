771c07d77e9406600e76ca019817bdf4036a7795
abhi6112 <abhi6112@users.noreply.github.com>
2025-09-01 14:25:55 +0530

    Uploaded the codes

---
+++ Codes/logistics/src/main/java/in/parthi/ui/TransactionInterface.java
@@ -0,0 +1,58 @@
+package in.parthi.ui;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import in.parthi.core.model.Transaction;
+import in.parthi.core.repository.TransactionRepo;
+
+public class TransactionInterface {
+    private static final Logger logger = LoggerFactory.getLogger(TransactionInterface.class);
+    TransactionRepo transactionRepo = new TransactionRepo();
+
+     /**
+     * This method take a transaction details and call repo class to add the transaction.
+     * 
+     * @return Returns a responce message for the addition action of transaction
+     */
+     public String addTransaction() {
+         String response = "";
+         logger.info("Start taking transaction details from user");
+         Scanner sc = new Scanner(System.in);
+         Transaction transaction = new Transaction();
+         try {
+             //Take transaction details from user
+             LocalDate today = LocalDate.now();
+             System.out.print("Enter Stockin Date e.g 2025-09-23 (default: " + today.toString() + "): ");
+             String strDate = sc.nextLine();
+             if (strDate.length() == 0) {
+                 transaction.setTransactionDate(today);
+             } else {
+                 transaction.setTransactionDate(LocalDate.parse(strDate));
+             }
+
+             transaction.setStatus("AVAILABLE");
+
+             System.out.print("Enter Transaction ID: ");
+             transaction.setId(sc.nextLine());
+
+             System.out.print("Enter Product ID: ");
+             transaction.setProductId(sc.nextLine());
+
+             System.out.print("Enter Customer ID: ");
+             transaction.setCustomerId(sc.nextLine());
+
+
+             response = transactionRepo.addTransaction(null);
+         } catch (RuntimeException e) {
+             response = e.getLocalizedMessage();
+             logger.error("Exception occured while adding transaction: " + e.getLocalizedMessage());
+         }finally {
+             sc.close();
+         }
+         return response;
+     }
+}
